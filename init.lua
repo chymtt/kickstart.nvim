13da1acfcb1a77df614e77bfce6152a967b0b9c5
Tu Do <angelwing1001@gmail.com>
2025-06-26 22:31:21 +0700

    update key map for blink

--- init.lua
+++ init.lua
@@ -710,8 +710,8 @@ require('lazy').setup({
         preset = 'default',
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        ['<c-j>'] = { 'select_prev', 'fallback' },
-        ['<c-k>'] = { 'select_next', 'fallback' },
+        ['<c-j>'] = { 'select_next', 'fallback' },
+        ['<c-k>'] = { 'select_prev', 'fallback' },
       },
 
       appearance = {
