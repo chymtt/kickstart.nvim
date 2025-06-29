e3c2da7ca2ad8182e1ad9b9a1260a5720c920158
Tu Do <angelwing1001@gmail.com>
2025-06-29 19:02:09 +0700

    update blink cmp config

--- init.lua
+++ init.lua
@@ -721,9 +721,16 @@ require('lazy').setup({
       },
 
       completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        menu = {
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind', gap = 1 },
+              { 'source_name' },
+            },
+          },
+        },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
       },
 
       sources = {
