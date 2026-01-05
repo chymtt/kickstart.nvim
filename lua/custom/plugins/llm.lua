4dc06e133e08dc40bbbb6aab7eed736d94d08b43
Tu Do <angelwing1001@gmail.com>
2026-01-05 11:28:41 +0700

    update lsp and add noice

--- lua/custom/plugins/llm.lua
+++ lua/custom/plugins/llm.lua
@@ -1,84 +1,84 @@
 return {
-  {
-    'yetone/avante.nvim',
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
-    build = function()
-      -- conditionally use the correct build system for the current OS
-      if vim.fn.has 'win32' == 1 then
-        return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
-      else
-        return 'make BUILD_FROM_SOURCE=true'
-      end
-    end,
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = 'claude',
-      auto_suggestions_provider = 'claude',
-      providers = {
-        claude = {
-          endpoint = 'https://api.anthropic.com',
-          -- model = 'claude-sonnet-4-20250514',
-          model = 'claude-3-7-sonnet-20250219',
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-      },
-      file_selector = {
-        provider = 'telescope',
-        -- Options override for custom providers
-        provider_opts = {},
-      },
-      selector = {
-        provider = 'telescope',
-        provider_opts = {},
-        exclude_auto_select = {}, -- List of items to exclude from auto selection
-      },
-      input = {
-        provider = 'snacks',
-      },
-    },
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      'folke/snacks.nvim', -- for input provider snacks
-      {
-        -- support for image pasting
-        'HakonHarnes/img-clip.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
-    },
-  },
+  -- {
+  --   'yetone/avante.nvim',
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   -- ⚠️ must add this setting! ! !
+  --   build = function()
+  --     -- conditionally use the correct build system for the current OS
+  --     if vim.fn.has 'win32' == 1 then
+  --       return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
+  --     else
+  --       return 'make BUILD_FROM_SOURCE=true'
+  --     end
+  --   end,
+  --   event = 'VeryLazy',
+  --   version = false, -- Never set this value to "*"! Never!
+  --   ---@module 'avante'
+  --   ---@type avante.Config
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     provider = 'claude',
+  --     auto_suggestions_provider = 'claude',
+  --     providers = {
+  --       claude = {
+  --         endpoint = 'https://api.anthropic.com',
+  --         -- model = 'claude-sonnet-4-20250514',
+  --         model = 'claude-3-7-sonnet-20250219',
+  --         timeout = 30000, -- Timeout in milliseconds
+  --         extra_request_body = {
+  --           temperature = 0.75,
+  --           max_tokens = 20480,
+  --         },
+  --       },
+  --     },
+  --     file_selector = {
+  --       provider = 'telescope',
+  --       -- Options override for custom providers
+  --       provider_opts = {},
+  --     },
+  --     selector = {
+  --       provider = 'telescope',
+  --       provider_opts = {},
+  --       exclude_auto_select = {}, -- List of items to exclude from auto selection
+  --     },
+  --     input = {
+  --       provider = 'snacks',
+  --     },
+  --   },
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     --- The below dependencies are optional,
+  --     'folke/snacks.nvim', -- for input provider snacks
+  --     {
+  --       -- support for image pasting
+  --       'HakonHarnes/img-clip.nvim',
+  --       event = 'VeryLazy',
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { 'markdown', 'Avante' },
+  --       },
+  --       ft = { 'markdown', 'Avante' },
+  --     },
+  --   },
+  -- },
   {
     'supermaven-inc/supermaven-nvim',
     config = function()
--- lua/custom/plugins/tailwind.lua
+++ lua/custom/plugins/tailwind.lua
@@ -1,9 +1,11 @@
 return {
-  'luckasRanarison/tailwind-tools.nvim',
-  build = ':UpdateRemotePlugins',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-telescope/telescope.nvim', -- optional
-  },
-  opts = {}, -- your configuration
+  -- {
+  --   'luckasRanarison/tailwind-tools.nvim',
+  --   build = ':UpdateRemotePlugins',
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-telescope/telescope.nvim', -- optional
+  --   },
+  --   opts = {}, -- your configuration
+  -- },
 }
